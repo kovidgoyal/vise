db727cbb6db05768f9429eea994d9a56b6ae603b
Kovid Goyal <kovid@kovidgoyal.net>
2015-08-30 08:44:55 +0530

    Run a single vise instance
    Open passed in urls in new tabs when running repeatedly from the command line

--- vise/main.py
+++ vise/main.py
@@ -7,11 +7,13 @@ import gc
 import os
 import sys
 import traceback
+import json
 from datetime import datetime
 from gettext import gettext as _
 
 from PyQt5.Qt import (
-    QApplication, QFontDatabase, QNetworkAccessManager, QNetworkDiskCache
+    QApplication, QFontDatabase, QNetworkAccessManager, QNetworkDiskCache,
+    QLocalSocket, QLocalServer, QSslSocket, QTextStream
 )
 
 from .constants import appname, str_version, cache_dir
@@ -20,7 +22,7 @@ from .settings import delete_profile
 from .window import MainWindow
 from .utils import parse_url
 
-app = None
+app = ADDRESS = None
 
 
 def option_parser():
@@ -33,6 +35,10 @@ class Application(QApplication):
 
     def __init__(self, args):
         QApplication.__init__(self, [])
+        if not QSslSocket.supportsSsl():
+            raise SystemExit('Qt has been compiled without SSL support!')
+
+        self.run_local_server(args)
         sys.excepthook = self.on_unhandled_error
         self.windows = []
         f = self.font()
@@ -47,6 +53,56 @@ class Application(QApplication):
         c.setMaximumCacheSize(10 * 1024 * 1024)
         nam.setCache(c)
 
+    def run_local_server(self, args):
+        server_name = appname + '-local-server'
+        s = QLocalSocket()
+        s.connectToServer(server_name)
+        if s.waitForConnected(500):
+            stream = QTextStream(s)
+            stream.setCodec('UTF-8')
+            cargs = json.dumps({'open': args.urls}, ensure_ascii=False)
+            stream << cargs
+            stream.flush()
+            s.waitForBytesWritten()
+            raise SystemExit(0)
+        self.local_server = ls = QLocalServer(self)
+        ls.newConnection.connect(self.another_instance_wants_to_talk)
+        if not ls.listen(server_name):
+            if ls.serverError() == QLocalSocket.AddressInUseError:
+                try:
+                    os.remove(ls.fullServerName())
+                except FileNotFoundError:
+                    pass
+            if not ls.listen(server_name):
+                raise SystemExit('Failed to establish local listening socket at: %s with error: %s' % (
+                    ls.errorString(), ls.fullServerName()))
+
+    def another_instance_wants_to_talk(self):
+        s = self.local_server.nextPendingConnection()
+        if s is None:
+            return
+        s.waitForReadyRead(1000)
+        stream = QTextStream(s)
+        stream.setCodec('UTF-8')
+        try:
+            command = json.loads(stream.readAll())
+        except Exception as e:
+            self.error('Invalid data from other instance: %s' % e)
+            return
+        finally:
+            s.close()
+            del s
+        if not isinstance(command, dict):
+            self.error('Invalid data from other instance: %r' % command)
+            return
+        urls = command.get('open', [])
+        if not isinstance(urls, list):
+            self.error('Invalid data from other instance: %r' % command)
+            return
+        urls = [x for x in urls if isinstance(x, str)]
+        if urls:
+            self.open_urls(urls, in_current_tab=False)
+
     def new_window(self, is_private=False):
         w = MainWindow(is_private=is_private)
         self.windows.append(w)
@@ -58,12 +114,12 @@ class Application(QApplication):
         except ValueError:
             pass
 
-    def open_urls(self, urls):
+    def open_urls(self, urls, in_current_tab=True):
         if not self.windows:
             self.new_window().show()
-        w = self.windows[0]
+        w = self.activeWindow() or self.windows[0]
         for i, url in enumerate(urls):
-            w.open_url(parse_url(url), in_current_tab=i == 0)
+            w.open_url(parse_url(url), in_current_tab=in_current_tab and i == 0)
 
     def error(self, *args, **kwargs):
         kwargs['file'] = sys.stderr
@@ -89,7 +145,8 @@ class Application(QApplication):
     def break_cycles(self):
         # Make sure the application object has no references in python and the
         # other global objects can also be garbage collected
-        del self.windows, self.network_access_manager
+        self.local_server.close()
+        del self.windows, self.network_access_manager, self.local_server
         sys.excepthook = sys.__excepthook__
 
 
