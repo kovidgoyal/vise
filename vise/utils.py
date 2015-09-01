4355f4a330c3a042c81bb4cd7eadb5e4554ccf98
Kovid Goyal <kovid@kovidgoyal.net>
2015-09-01 12:35:19 +0530

    Use Qt's url from user input routines

--- vise/utils.py
+++ vise/utils.py
@@ -2,7 +2,7 @@
 # vim:fileencoding=utf-8
 # License: GPL v3 Copyright: 2015, Kovid Goyal <kovid at kovidgoyal.net>
 
-import re
+import os
 import math
 from functools import lru_cache
 
@@ -12,13 +12,9 @@ from PyQt5.Qt import (
 
 from .settings import gprefs
 
-upat = re.compile(r'[a-zA-Z0-9]+://')
-
 
 def parse_url(url_or_path):
-    if upat.match(url_or_path) is not None:
-        return QUrl(url_or_path)
-    return QUrl.fromLocalFile(url_or_path)
+    QUrl.fromUserInput(url_or_path, os.getcwd())
 
 
 @lru_cache(maxsize=500)
