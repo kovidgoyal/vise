36427b47fc1f8651d57117c493146ed930ef4188
Kovid Goyal <kovid@kovidgoyal.net>
2015-12-25 10:56:19 +0530

    Finish password store implementation

--- vise/passwd.py
+++ vise/passwd.py
@@ -6,10 +6,16 @@ import json
 import os
 import hashlib
 import struct
+import tempfile
+from binascii import hexlify, unhexlify
 from threading import Thread
 
-from .crypto import derive_key_v1, generate_salt_v1, nonce_size_v1, decrypt_v1, encrypt_v1, lock_python_bytes
+from .crypto import derive_key_v1, generate_salt_v1, nonce_size_v1, decrypt_v1, encrypt_v1, lock_python_bytes, MessageForged
 from .utils import atomic_write
+
+
+class PasswordWrong(ValueError):
+    pass
 
 
 class PasswordStore:
@@ -30,9 +36,8 @@ class PasswordStore:
         except FileNotFoundError:
             self.metadata = {
                 'version': 1,
-                'salt': generate_salt_v1()
+                'salt': hexlify(generate_salt_v1()).decode('ascii')
             }
-            self.commit_metadata()
         self.key = self.key_error = None
         self.password = password
         self.derive_worker = Thread(name='DeriveKey', target=self.derive_key)
@@ -46,7 +51,17 @@ class PasswordStore:
     def derive_key(self):
         pw, self.password = self.password, None
         try:
-            self.key = derive_key_v1(pw, self.metadata['salt'])[0]
+            self.key = derive_key_v1(pw, unhexlify(self.metadata['salt']))[0]
+            if 'sentinel' not in self.metadata:
+                cipher, nonce = encrypt_v1(b'sentinel', self.key)
+                self.metadata['sentinel'] = (hexlify(nonce).decode('ascii'), hexlify(cipher).decode('ascii'))
+                self.commit_metadata()
+            else:
+                nonce, cipher = unhexlify(self.metadata['sentinel'][0]), unhexlify(self.metadata['sentinel'][1])
+                try:
+                    decrypt_v1(cipher, nonce, self.key)
+                except MessageForged:
+                    raise PasswordWrong('The password is incorrect')
         except Exception as e:
             self.key_error = e
 
@@ -55,12 +70,18 @@ class PasswordStore:
             key = key.encode('utf-8')
         return hashlib.sha1(key).hexdigest()
 
-    def get_data(self, key):
+    def join(self):
         self.derive_worker.join()
         if self.key_error is not None:
             raise self.key_error
+
+    def get_data(self, key):
+        self.join()
         fname = self.generate_file_name(key)
-        return self.read_data(fname)[1]
+        try:
+            return self.read_data(fname)[1]
+        except MessageForged:
+            raise ValueError('The data for %s is corrupted' % key)
 
     def __iter__(self):
         for entry in os.scandir(self.root):
@@ -70,9 +91,7 @@ class PasswordStore:
                     yield name
 
     def get_all_data(self):
-        self.derive_worker.join()
-        if self.key_error is not None:
-            raise self.key_error
+        self.join()
         for name in self:
             try:
                 key, data = self.read_data(name)
@@ -82,24 +101,33 @@ class PasswordStore:
                 yield key, data
 
     def set_data(self, key, data):
-        self.derive_worker.join()
-        if self.key_error is not None:
-            raise self.key_error
+        self.join()
         fname = self.generate_file_name(key)
         if isinstance(key, str):
             key = key.encode('utf-8')
         if isinstance(data, str):
             data = data.encode('utf-8')
-        data, nonce = encrypt_v1(key + data, self.key)
+        keysize = struct.pack('!I', len(key))
+        data, nonce = encrypt_v1(keysize + key + data, self.key)
         atomic_write(os.path.join(self.root, fname), nonce + data)
 
     def read_data(self, fname):
         try:
-            with open(os.path.join(self.root, fname), 'wb') as f:
+            with open(os.path.join(self.root, fname), 'rb') as f:
                 nonce = f.read(nonce_size_v1())
-                data = memoryview(decrypt_v1(f.read(), nonce, self.key))
+                data = decrypt_v1(f.read(), nonce, self.key)
                 keysize, = struct.unpack_from('!I', data)
-                key = data[:keysize].decode('utf-8')
-                return key, data[keysize + struct.calcsize('!I'):]
+                offset = struct.calcsize('!I')
+                key = data[offset:keysize + offset].decode('utf-8')
+                return key, data[keysize + offset:]
         except FileNotFoundError:
             return None, None
+
+
+def test():
+    with tempfile.TemporaryDirectory() as tdir:
+        p = PasswordStore(tdir, 'test')
+        p.join()
+        p.set_data('a', 'one')
+        assert p.get_data('a') == b'one'
+        assert tuple(p.get_all_data()) == (('a', b'one'),)
