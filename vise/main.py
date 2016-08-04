97f92fc27f7081e994cfd6cdaa23aa302dd4ae69
Kovid Goyal <kovid@kovidgoyal.net>
2016-08-04 05:41:16 +0530

    ...

--- vise/main.py
+++ vise/main.py
@@ -345,7 +345,7 @@ def restart(state, env):
 def run_app(urls=(), callback=None, callback_wait=0, master_password=None, new_instance=False, shutdown=False, restart_state=None):
     env = os.environ.copy()
     # Workaround for: https://bugreports.qt.io/browse/QTBUG-55125
-    if 'TZ' not in os.eviron and os.path.exists('/etc/localtime'):
+    if 'TZ' not in os.environ and os.path.exists('/etc/localtime'):
         os.environ['TZ'] = '/etc/localtime'
     app = Application([], master_password=master_password, urls=urls, new_instance=new_instance, shutdown=shutdown, restart_state=restart_state)
     if False:
