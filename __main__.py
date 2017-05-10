9ea3cc42ae4c91f7f269a5460d013ba893a7477b
Kovid Goyal <kovid@kovidgoyal.net>
2017-05-10 14:21:27 +0530

    Nicer error message when qt-webengine is missing

--- __main__.py
+++ __main__.py
@@ -2,6 +2,12 @@
 # vim:fileencoding=utf-8
 # License: GPL v3 Copyright: 2015, Kovid Goyal <kovid at kovidgoyal.net>
 
+try:
+    import PyQt5.QtWebEngine as dummy
+    del dummy
+except ImportError:
+    raise SystemExit(
+        'Your system appears to be missing the qt-webengine package')
 
 from vise.main import main
 main()
