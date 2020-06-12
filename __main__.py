b46d4e8d7e4974163500dd98a54935b1c5a6bb93
Kovid Goyal <kovid@kovidgoyal.net>
2020-06-12 09:25:56 +0530

    Add a --force-dark-mode command line flag

--- __main__.py
+++ __main__.py
@@ -3,12 +3,25 @@
 # License: GPL v3 Copyright: 2015, Kovid Goyal <kovid at kovidgoyal.net>
 
 import os
+import sys
 
-if 'auto_proxy' in os.environ:
+from vise.main import main
+
+
+def add_webengine_flag(flag):
     val = os.environ.get('QTWEBENGINE_CHROMIUM_FLAGS', '')
     if val:
         val += ' '
-    os.environ['QTWEBENGINE_CHROMIUM_FLAGS'] = '--proxy-pac-url=' + os.environ['auto_proxy']
+    os.environ['QTWEBENGINE_CHROMIUM_FLAGS'] = val + flag
+
+
+while '--force-dark-mode' in sys.argv:
+    add_webengine_flag('--force-dark-mode')
+    sys.argv.remove('--force-dark-mode')
+
+if 'auto_proxy' in os.environ:
+    add_webengine_flag('--proxy-pac-url=' + os.environ['auto_proxy'])
+
 try:
     import PyQt5.QtWebEngine as dummy
     del dummy
@@ -16,5 +29,4 @@ except ImportError:
     raise SystemExit(
         'Your system appears to be missing the qt-webengine package')
 
-from vise.main import main
 main()
